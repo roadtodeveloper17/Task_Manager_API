60cab49b03cfe0960b01fcc161ff126d219d68e5
Sayat Turgynbay <sayatturgynbay123@gmail.com>
2026-06-26 17:32:52 +0500

    config.py added and __init__.py added

---
+++ app/core/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    app_name: str = "Task Manager API"
+    environment: str = "development"
+    database_url: str = "postgresql://postgres:postgres@localhost:5432/taskflow_db"
+    test_database_url: str = "postgresql://postgres:postgres@localhost:5432/test_taskflow_db"
+    secret_key: str = "my_secret_key"
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
+    redis_url: str = "redis://localhost:6379/0"
+    celery_broker_url: str = "redis://localhost:6379/1"
+    celery_backend_url: str = "redis://localhost:6379/2"
+
+settings = Settings()
